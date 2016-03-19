ca3df26aabdfff5f029a03ba98a135790eaa23d6
Aswin Radhakrishnan <aswinrkrishnan@gmail.com>
2016-03-20 01:26:41 +1100

    hardcoded values: temp fix to finalise layout

--- app/controllers/chronos_controller.rb
+++ app/controllers/chronos_controller.rb
@@ -1,13 +1,17 @@
 class ChronosController < ApplicationController
 	before_filter :authenticate_user!
   def home
-  	@jobCount = 10
+  	getStats
   end
 
   def getJobs
   end
 
   def getStats
+  	@failedJobs  = 10
+  	@successJobs = 10
+  	@idleJobs    = 10
+  	@totalJob    = @successJobs + @failedJobs + @idleJobs 
   end
 
   def getJobInfo
