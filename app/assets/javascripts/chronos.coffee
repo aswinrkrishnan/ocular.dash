71504a8658d5e67b03b1fc1fbb791e26cbce8f69
Aswin Radhakrishnan <aswinrkrishnan@gmail.com>
2016-03-21 10:27:22 +1100

    testing with parsing JSON in chronos

--- app/assets/javascripts/chronos.coffee
+++ app/assets/javascripts/chronos.coffee
@@ -2,8 +2,6 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
-alert("hi")
-
 # $('#jobList tr th').click(function(){
 #   $("#jobList tr th i").removeClass();
 #   $("#jobList tr th i").addClass("fa fa-sort");
--- app/controllers/chronos_controller.rb
+++ app/controllers/chronos_controller.rb
@@ -1,10 +1,16 @@
 class ChronosController < ApplicationController
 	before_filter :authenticate_user!
+    require 'json'
   def home
   	getStats
+  	getJobs
   end
 
   def getJobs
+  	@json = '[{"name": "job1","time": "10AM","status": "success"}, {"name": "job2","time": "11AM","status": "failed"},
+  	{"name": "job3","time": "10AM","status": "idle"}, {"name": "job5","time": "10AM","status": "success"},
+  	{"name": "job4","time": "10AM","status": "idle   "}, {"name": "job6","time": "10AM","status": "failed"}]'
+  	@jobList = JSON.parse(@json)
   end
 
   def getStats
