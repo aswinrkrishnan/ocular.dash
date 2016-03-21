765dc1da263e86297603d06103c4223ac64f1899
Aswin Radhakrishnan <aswinrkrishnan@gmail.com>
2016-03-21 23:42:18 +1100

    integrated getting job details for chronos when a job is clicked

--- app/views/chronos/_chronos.js
+++ app/views/chronos/_chronos.js
@@ -23,6 +23,7 @@ $("#job_search").keyup(function() {
     }
  });
 });
+})
 function timeDifference(previous) {
     if(previous == ""){return "No Previous runs";}
     var msPerMinute = 60 * 1000;
@@ -34,7 +35,6 @@ function timeDifference(previous) {
     else if (elapsed < msPerDay ) return Math.round(elapsed/msPerHour ) + ' hours ago';
     else return new Date(previous)+"";
 }
-})
 function filterTable(value){
   if(value == 'all'){
     $('#jobList tbody tr').show();
@@ -43,4 +43,27 @@ function filterTable(value){
     $('#jobList tbody tr').hide();
     $('#jobList tr td:contains("'+value+'")').parent().toggle();
   }
+}
+function showJobDetails(jobDetail){
+	var job = $(jobDetail).find(".jobDescription").data('job')
+	$("#jobDetails").modal('show');
+	$("#jobDetails #jobName").text(job.name);
+    $("#jobDetails #description").text(job.description);
+    $("#jobDetails #command").text(job.command);
+    $("#jobDetails #owner").text(job.owner);
+    $("#jobDetails #lastSuccess").text(timeDifference(job.lastSuccess));
+    $("#jobDetails #successCount").text(job.successCount);
+    $("#jobDetails #errorCount").text(job.errorCount);
+    $("#jobDetails #lastError").text(timeDifference(job.lastError))
+    $("#jobDetails #schedule").text(job.schedule)
+    $('#volInfoTable').find("tr:gt(0)").remove();
+    for(var i = 0; i < job.container.volumes.length; i++ ){
+        var ico="";
+        if(job.container.volumes[i].mode=="RO")
+            ico="<i class='fa fa-lock'></i>"
+        else
+            ico="<i class='fa fa-unlock'></i>"
+    $('#volInfoTable > tbody:last-child').append('<tr><td>'+job.container.volumes[i].hostPath+'</td><td>'+job.container.volumes[i].containerPath+'</td><td><div class="badge bg-blue">'+ico+job.container.volumes[i].mode+'</div></td></tr>');
+
+}
 }
