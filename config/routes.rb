81dfd0194cabae63139dc01e78e7b5ae0af6cf66
Aswin Radhakrishnan <aswinrkrishnan@gmail.com>
2016-05-15 12:06:45 +1000

    Error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   root 'chronos#home'
 
   get 'chronos/home'
@@ -26,58 +30,10 @@ Rails.application.routes.draw do
 
   get 'autosys/getJobLog'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  match "/404", :to => "errors#not_found", :via => :all
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  match "*path", :to => "errors#not_found", :via => :all
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
